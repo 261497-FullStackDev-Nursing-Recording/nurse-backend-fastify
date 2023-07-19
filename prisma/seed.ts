0a958802f98ea8b24456d12dd99f9061ab4c02d8
Mhingzzz <90751090+Mhingzzz@users.noreply.github.com>
2023-07-19 21:12:07 +0700

    implement some data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,48 +9,55 @@ async function main() {
     app.register(fastifyBcrypt);
     await app.ready();
 
-    // const user = await prisma.user.create({
-    //     data: {
-    //         f_name: 'User1',
-    //         l_name: 'User1',
-    //         username: 'uniqe',
-    //         password: await app.bcrypt.hash('password'),
-    //         role: 'NURSE',
-    //     },
-    // });
-
-    const _ = await prisma.record.createMany({
+    /**
+     * create a patients in the database if you don't need it just comment it
+     */
+    const patients = await prisma.patient.createMany({
         data: [
             {
-                user_id: 'a00178d4-ceed-4846-88c2-0139bb268723',
-                patient_id: '37973c7f-44f2-435a-b4b3-525df1e9580b',
-                bed_number: 1,
-                ward: 'ward',
-                diagnose: 'diagnose',
-                shift: 'EVENING',
-                visit_number: 'visit_number',
+                f_name: 'Patients',
+                l_name: 'Bed1',
+                hn: '001',
+                status: 'STATUS_1',
             },
             {
-                user_id: 'a00178d4-ceed-4846-88c2-0139bb268723',
-                patient_id: '37973c7f-44f2-435a-b4b3-525df1e9580b',
-                bed_number: 2,
-                ward: 'ward',
-                diagnose: 'diagnose',
-                shift: 'EVENING',
-                visit_number: 'visit_number',
+                f_name: 'Patients',
+                l_name: 'Bed2',
+                hn: '002',
+                status: 'STATUS_2',
+            },
+            {
+                f_name: 'Patients',
+                l_name: 'Bed3',
+                hn: '003',
+                status: 'STATUS_3',
             },
         ],
     });
 
-    // const patient = await prisma.patient.create({
-    //     data: {
-    //         f_name: 'f_nurse',
-    //         l_name: 'l_nurse',
-    //         hn: 'hn',
-    //         status: 'STATUS_1',
-    //     },
+    // const records = await prisma.record.createMany({
+    //     data: [
+    //         {
+    //             user_id: 'a00178d4-ceed-4846-88c2-0139bb268723',
+    //             patient_id: '37973c7f-44f2-435a-b4b3-525df1e9580b',
+    //             bed_number: 1,
+    //             ward: 'ward',
+    //             diagnose: 'diagnose',
+    //             shift: 'EVENING',
+    //             visit_number: 'visit_number',
+    //         },
+    //         {
+    //             user_id: 'a00178d4-ceed-4846-88c2-0139bb268723',
+    //             patient_id: '37973c7f-44f2-435a-b4b3-525df1e9580b',
+    //             bed_number: 2,
+    //             ward: 'ward',
+    //             diagnose: 'diagnose',
+    //             shift: 'EVENING',
+    //             visit_number: 'visit_number',
+    //         },
+    //     ],
     // });
-    console.log('records created:', _);
+    console.log('create' + patients);
 }
 
 main()
