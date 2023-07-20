ec01da2b12c5ee1bfe0ac80b1f0a98c4945f5972
Nuttapol Saithong <Nonokub.671@gmail.com>
2023-07-20 19:46:35 +0700

    fix records api

--- src/routes/records/index.ts
+++ src/routes/records/index.ts
@@ -3,7 +3,6 @@ import { FastifyPluginAsync } from 'fastify';
 
 const GetRecordsResDTO = Type.Array(
     Type.Object({
-        id: Type.String(),
         user_id: Type.String(),
         patient_id: Type.String(),
         bed_number: Type.Integer(),
@@ -13,6 +12,44 @@ const GetRecordsResDTO = Type.Array(
         visit_number: Type.String(),
         created_at: Type.String(),
         modified_at: Type.String(),
+    }),
+);
+const GetRecordsInformationResDTO = Type.Array(
+    Type.Object({
+        user_id: Type.String(),
+        patient_id: Type.String(),
+        bed_number: Type.Integer(),
+        ward: Type.String(),
+        diagnose: Type.String(),
+        shift: Type.String(),
+        visit_number: Type.String(),
+        created_at: Type.String(),
+        modified_at: Type.String(),
+        a_field: Type.Array(
+            Type.Object({
+                message: Type.String(),
+            }),
+        ),
+        e_field: Type.Array(
+            Type.Object({
+                message: Type.String(),
+            }),
+        ),
+        i_field: Type.Array(
+            Type.Object({
+                message: Type.String(),
+            }),
+        ),
+        o_field: Type.Array(
+            Type.Object({
+                message: Type.String(),
+            }),
+        ),
+        s_field: Type.Array(
+            Type.Object({
+                message: Type.String(),
+            }),
+        ),
     }),
 );
 
@@ -36,6 +73,31 @@ const records: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
         handler: async (request, reply) => {
             try {
                 const records = await fastify.prisma.record.findMany();
+                return records;
+            } catch (err) {
+                reply.code(500).send(err);
+            }
+        },
+    });
+    fastify.route({
+        method: 'GET',
+        url: '/info',
+        schema: {
+            response: {
+                200: GetRecordsInformationResDTO,
+            },
+        },
+        handler: async (request, reply) => {
+            try {
+                const records = await fastify.prisma.record.findMany({
+                    include: {
+                        a_field: true,
+                        e_field: true,
+                        i_field: true,
+                        o_field: true,
+                        s_field: true,
+                    },
+                });
                 return records;
             } catch (err) {
                 reply.code(500).send(err);
