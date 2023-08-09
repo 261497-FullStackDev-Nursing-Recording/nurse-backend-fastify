6bc53f8bb763a9c076bc1d66e1af73f3e07b7a37
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-08-09 11:11:47 +0700

    First PDF-generation prototype
    Squashed commit of the following:
    
    commit 3488bb756c1902b39b99f4107481604f3036970f
    Author: Nirand Pisutha-Arnond <nnnpooh@gmail.com>
    Date:   Wed Aug 9 11:10:55 2023 +0700
    
        Finish first prototype pdf-gen
    
    commit 6c4583dc3f37efbee4e04405f26357bcbe5133cd
    Author: Nirand Pisutha-Arnond <nnnpooh@gmail.com>
    Date:   Wed Aug 9 10:35:21 2023 +0700
    
        Update
    
    commit a1a3d4878865283fbb14359f77dc1f678920560c
    Author: Nirand Pisutha-Arnond <nnnpooh@gmail.com>
    Date:   Wed Aug 9 09:11:26 2023 +0700
    
        Update table
    
    commit 35a5dca002bfa700d1ae3454c7face9aa094e6e7
    Author: Nirand Pisutha-Arnond <nnnpooh@gmail.com>
    Date:   Wed Aug 9 08:18:29 2023 +0700
    
        Add font
    
    commit 89ee737e7c5960146e012d68a9949ad97d0cd5d5
    Author: Nirand Pisutha-Arnond <nnnpooh@gmail.com>
    Date:   Wed Aug 9 07:16:35 2023 +0700
    
        Update
    
    commit 4b07b6ef7d06283534be3226f961858745fd7805
    Author: Nirand Pisutha-Arnond <nnnpooh@gmail.com>
    Date:   Wed Aug 9 05:33:46 2023 +0700
    
        Still cannot use tailwind
    
    commit 32f945b2b7d77dac7218ad7f1351cd816210d153
    Author: Nirand Pisutha-Arnond <nnnpooh@gmail.com>
    Date:   Wed Aug 9 05:20:06 2023 +0700
    
        Working on tailwind
    
    commit 73e97ed44005f564eec243a439269ed337e659aa
    Author: Nirand Pisutha-Arnond <nnnpooh@gmail.com>
    Date:   Wed Aug 9 05:04:42 2023 +0700
    
        Experimenting with template engine
    
    commit 3d8614f54bb0a6e086d9d076cc095d8ab70132de
    Author: Nirand Pisutha-Arnond <nnnpooh@gmail.com>
    Date:   Tue Aug 8 10:06:46 2023 +0700
    
        Working on pdf gen

---
+++ pdf/pdf-gen.ts
@@ -0,0 +1,81 @@
+import { type PDFOptions } from 'puppeteer';
+
+import { datas } from './data';
+
+const puppeteer = require('puppeteer');
+const handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
+
+const main = async () => {
+    const templateHtml = fs.readFileSync(
+        path.join(__dirname, 'templates', 'template1.html'),
+        'utf8',
+    );
+    const template = handlebars.compile(templateHtml);
+    console.log(JSON.stringify(datas));
+    const html = template({ datas });
+    fs.writeFileSync(path.join(__dirname, 'templates', 'output.html'), html);
+
+    const pdfPath = path.join(
+        __dirname,
+        'output',
+        `Test-${new Date().getTime()}.pdf`,
+    );
+
+    const options: PDFOptions = {
+        // width: '1230px',
+        headerTemplate: '<p>Header</p>',
+        footerTemplate: '<p>Footer</p>',
+        format: 'A4',
+        displayHeaderFooter: false,
+        margin: {
+            top: '1cm',
+            bottom: '1cm',
+            left: '1cm',
+            right: '1cm',
+        },
+        printBackground: true,
+        path: pdfPath,
+        landscape: true,
+    };
+
+    // launch a new chrome instance
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
+        headless: 'new',
+    });
+
+    // create a new page
+    const page = await browser.newPage();
+
+    // set your html as the pages content
+    await page.setContent(html, {
+        waitUntil: 'domcontentloaded',
+    });
+
+    // await page.goto(`data:text/html;charset=UTF-8,${html}`, {
+    //     waitUntil: 'networkidle0',
+    // });
+
+    await page.addStyleTag({
+        path: path.join(__dirname, 'templates', 'styles', 'style.css'),
+    });
+
+    await page.addStyleTag({
+        path: path.join(__dirname, 'templates', 'styles', 'font.css'),
+    });
+
+    // create a pdf buffer
+    // const pdfBuffer = await page.pdf({
+    //     format: 'A4',
+    // });
+
+    // or a .pdf file
+    await page.pdf(options);
+
+    // close the browser
+    await browser.close();
+};
+
+main();
