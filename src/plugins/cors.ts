e945842f3203d1045bc368b832ee8134a29568e4
Nuttapol Saithong <Nonokub.671@gmail.com>
2023-09-26 21:00:52 +0700

    fix domain

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -3,7 +3,7 @@ import fp from 'fastify-plugin';
 
 export default fp(async (fastify) => {
     fastify.register(cors, {
-        origin: 'localhost:3000',
+        origin: 'http://localhost:3000',
         credentials: true,
     });
 });
